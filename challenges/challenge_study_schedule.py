b3e053836d790d7b62942da32a72f286f26e3d60
Thiesa <thiesa.c@gmail.com>
2023-10-21 14:21:36 -0400

    feat: Req1 - studdy_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,21 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    if (
+        target_time is None
+        or not isinstance(target_time, int)
+        or not (0 <= target_time <= 24)
+    ):
+        return None
+    for period in permanence_period:
+        if any(
+            x is None or not isinstance(x, int) or not (0 <= x <= 24)
+            for x in period
+        ):
+            return None
+        elif target_time in range(period[0], period[1] + 1):
+            counter += 1
+    return counter
+    # raise NotImplementedError
+
+
+# print(study_schedule([(75, 12), (11, 13), (7, 12), (10, 15)], 12))
