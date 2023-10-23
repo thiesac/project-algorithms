9b3e2376f089309e9bb4a9f71cde031398d7200a
Thiesa <thiesa.c@gmail.com>
2023-10-23 18:56:35 -0400

    feat: Req3 - palindrome recursive

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    # word = word.lower()
+    # reversed_word = ""
+
+    # if word == "":
+    #     return False
+    # else:
+    #     for i in range(len(word), 0, -1):
+    #         reversed_word += word[i - 1]
+    #     return word == reversed_word
     raise NotImplementedError
+
+
+# print(is_palindrome_iterative("coxinha"))
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    word = word.lower()
+    if word == "":
+        return False
+    elif high_index - low_index < 1:
+        return True
+    else:
+        return word[low_index] == word[high_index] and is_palindrome_recursive(
+            word, low_index + 1, high_index - 1
+        )
+
+
+print(is_palindrome_recursive("coxinha", 0, len("coxinha") - 1))
+print(is_palindrome_recursive("radar", 0, len("radar") - 1))
